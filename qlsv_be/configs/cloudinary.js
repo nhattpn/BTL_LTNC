d5ec868a0e7833b8fd5f565c139eb0c61d68e9ce
nhattpn <nhat.trantpn2212@hcmut.edu.vn>
2024-05-05 20:38:47 +0700

    finish

--- qlsv_be/configs/cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require('cloudinary').v2;
-          
-cloudinary.config({ 
-  cloud_name: 'doxxh59tp', 
-  api_key: '251629438694585', 
-  api_secret: 'wrFT5lTCc6L4cpoWk-cmAcJHJhk' 
-});
-
-module.exports = cloudinary
